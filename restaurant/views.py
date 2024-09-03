95ff35719950b17993223bb9f3e00cae43875879
michael Walker <codemikewalker@gmail.com>
2024-09-03 14:29:42 -0400

    add menu views, serializers for booking and menu

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import generics
+from .serializers import menuSerializer
+from .models import Menu
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+  
+class MenutItemView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = menuSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = menuSerializer
+
