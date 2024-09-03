faae6c43c9b967471b8bae34bd8f97e26647bcc2
michael Walker <codemikewalker@gmail.com>
2024-09-03 16:56:13 -0400

    url.py

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework import generics
-from .serializers import menuSerializer
-from .models import Menu
+from .serializers import menuSerializer, bookingSerializer
+from .models import Menu, Booking
+from rest_framework.viewsets import ModelViewSet
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
 
   
-class MenutItemView(generics.ListCreateAPIView):
+class MenuItemView(generics.ListCreateAPIView):
     queryset = Menu.objects.all()
     serializer_class = menuSerializer
 
@@ -17,3 +18,7 @@ class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView
     queryset = Menu.objects.all()
     serializer_class = menuSerializer
 
+
+class BookingViewSet(ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = bookingSerializer
